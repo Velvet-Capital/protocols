28b0e32deec2c596daee1d24e8a5d7917dc9d9c7
langnavina97 <81016364+langnavina97@users.noreply.github.com>
2022-06-27 12:27:43 -0300

    check if vtoken and underlying token pair exists on venus before adding pair to token metadata

--- contracts/venus/TokenMetadata.sol
+++ contracts/venus/TokenMetadata.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.4 || ^0.7.6 || ^0.8.0;
 
+import "./ComptrollerInterface.sol";
+import "./VBep20Storage.sol";
+
 contract TokenMetadata {
     mapping(address => address) public vTokens;
 
     function add(address _underlying, address _vToken) public {
-        // TODO check if _vToken is a valid vToken
+        ComptrollerInterface comptroller = ComptrollerInterface(
+            0xfD36E2c2a6789Db23113685031d7F16329158384
+        );
+        (bool isvToken, ) = comptroller.markets(_vToken);
+        VBep20Storage vToken = VBep20Storage(_vToken);
+        require(vToken.underlying() == _underlying);
+        require(isvToken, "vToken does not exist");
         require(vTokens[_underlying] != _vToken, "Pair already exists!");
         vTokens[_underlying] = _vToken;
     }
---
+++ contracts/venus/VBep20Storage.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.6;
+
+contract VBep20Storage {
+    /**
+     * @notice Underlying asset for this VToken
+     */
+    address public underlying;
+}
